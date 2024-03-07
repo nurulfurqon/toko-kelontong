38c6156035dcbc6b983e33ae0817d498df67274e
Nurul Furqon <furqon.mascode@gmail.com>
2024-03-07 19:51:58 +0700

    fix: remove unused file

--- pages/terms-of-service.vue
+++ pages/terms-of-service.vue
@@ -1,10 +1,4 @@
-<script lang="ts" setup>
-import { listProducts } from '~/dummy'
-</script>
+<script lang="ts" setup></script>
 <template>
-  <div class="w-full min-h-screen pt-64 flex items-center justify-center">
-    <div class="w-[360px]">
-      <CCarouselProducts :list-products="listProducts" />
-    </div>
-  </div>
+  <div class="w-full min-h-screen pt-64 flex items-center justify-center"></div>
 </template>
